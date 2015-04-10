d7a119b4cdcbee0d306dadb201659740b75c9062
Roechi <msg.remus@gmail.com>
2015-04-10 10:06:50 +0200

    Adapted application actual usage.

--- kindle/display-weather.sh
+++ kindle/display-weather.sh
@@ -6,7 +6,7 @@ rm weather-script-output.png
 eips -c
 eips -c
 
-if wget http://192.168.2.158/weather-script-output.png; then
+if wget http://192.168.178.65/weather-script-output.png; then
 	eips -g weather-script-output.png
 else
 	eips -g weather-image-error.png
--- weather-script.sh
+++ weather-script.sh
@@ -3,6 +3,6 @@
 cd "$(dirname "$0")"
 
 python fetch_weather.py
-rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
-pngcrush -c 0 -ow weather-script-output.png
-# cp -f weather-script-output.png /path/to/web/server/directory/weather-script-output.png
+rsvg-convert --background-color=white -o weather-script-output-pre.png weather-script-output.svg
+pngcrush -c 0 weather-script-output-pre.png weather-script-output.png
+cp -f weather-script-output.png /var/www/weather-script-output.png
